4d9c7072c66d513d9f4a95585981c628536adada
Andrew Lukyanov <nefesh.samael@gmail.com>
2021-09-22 15:36:38 +0300

    Plus method fix

--- HW1/Bucket.rb
+++ HW1/Bucket.rb
@@ -12,16 +12,17 @@ class Bucket
   def add_item(item)
     if PRICES.include?(item)
       @items << item
-    else puts " Error! You can add only: #{PRICES.keys}"
+    else " Error! You can add only: #{PRICES.keys}"
     end
   end
 
   def total_sum
-    puts self.items.each.map{|el| el = PRICES[el]}.sum
+    self.items.each.map{|el| el = PRICES[el]}.sum
   end
 
   def +(bucket)
     self.items.concat bucket.items
     bucket.items.clear
+    return self
   end
 end
